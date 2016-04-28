7395a4adf43233674b591b68961a3c9fa12d7d8a
aliciakw <alicia.willett@gmail.com>
2016-04-28 00:34:53 -0400

    Api data.

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express'),
     path = require('path'),
     app = express(),
-    port = 3000;
+    port = 3000,
+    questions = require('./data/questions.json'),
+    therapists = require('./data/therapists.json');
 
 app.use(express.static(path.join(__dirname, 'app')));
-// app.get('/api/channel/1', function(req, res) {
-//   res.json(data);
-// });
+app.get('/api/questions', function(req, res) {
+  res.json(questions);
+});
+app.get('/api/therapists', function(req, res) {
+  res.json(therapists);
+});
 app.listen(port, function () {
   console.log('Express server listening on port ' + port);
 });
